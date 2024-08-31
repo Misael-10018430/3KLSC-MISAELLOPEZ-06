42e9071e53c19710755d64319d50ed60189b145b
MISAEL L.A. <misa690la90@outlook.com>
2024-08-30 22:17:07 -0600

    Agregar archivos de proyecto.

---
+++ 3KLSC-MISAELLOPEZ-06/3KLSC-MISAELLOPEZ-06.cpp
@@ -0,0 +1,36 @@
+// 3KLSC-MISAELLOPEZ-06.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	vector<string> aves = { "Loro gris", "Paloma de diamante", "Coctel" };
+
+	cout << "Los valores del vector antes de insertar: n "; 
+
+	for (int i = 0; i < aves.size(); ++i) {
+
+		cout << aves[i] << " ";
+	}
+	cout << endl << endl;
+
+	aves.push_back("Mayna");
+	aves.push_back("Periquitos");
+	aves.push_back("Cacatua");
+
+	cout << "Los valores del vetor despues de insertar:\n";
+
+	for (int i = 0; i < aves.size(); ++i) {
+
+		cout << aves[i] << " ";
+
+	}
+	cout << endl;
+	return 0; 
+
+
+
+}
